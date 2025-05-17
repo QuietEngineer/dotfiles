737e55f1c17d04ce195b28cf46f7dfb175e1589e
QuietEngineer <qtengineer@proton.me>
2025-05-17 15:14:06 -0600

    feat: add misc features (#3)
    * feat: add zsh configs
    
    * fix: use correct link in README
    
    * feat: add atuin config
    
    * feat: add ghostty config

---
+++ zsh/.zsh/40-env.zsh
@@ -0,0 +1,20 @@
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+
+if which nvim &>/dev/null; then
+    export EDIT=${EDIT:-nvim}
+    alias vim=nvim
+elif which vim &>/dev/null; then
+    export EDIT=${EDIT:-vim}
+elif which vi &>/dev/null; then
+    export EDIT=${EDIT:-vi}
+    alias vim=vi
+else
+    [[ -n "$EDIT" ]] && export EDIT
+    alias nvim="echo could not find Neovim on this system"
+    alias vim="echo could not find Vim or vi on this system"
+    alias vi="echo could not find Vim or vi on this system"
+fi
+
+export EDITOR=$EDIT
