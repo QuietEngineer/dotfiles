05ded13a1a366bc2703e86731a4ce7b26a3cee0f
QuietEngineer <qtengineer@proton.me>
2025-05-17 21:18:33 -0600

    feat: add lazyvim configs (#4)
    * feat: add lazyvim setup
    
    * feat: sync clipboard
    
    * refactor: use lua instead of vimscript

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.o.signcolumn = 'yes'
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.shiftwidth=4
+
+vim.o.scrolloff = 5
+
+vim.o.spelllang = 'en_us'
+
+-- sync with system clipboard
+vim.o.clipboard = 'unnamed'
