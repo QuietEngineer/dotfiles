324f5033c277bc4f3ac610191b9db103df2b7432
QuietEngineer <qtengineer@proton.me>
2026-06-26 10:45:10 -0600

    feat: add conditional completions

--- zsh/.zsh/60-completion.zsh
+++ zsh/.zsh/60-completion.zsh
@@ -1,3 +1,27 @@
+[[ $commands[atuin] ]] && source <(atuin gen-completions --shell zsh)
+[[ $commands[jj] ]] && source <(COMPLETE=zsh jj)
+[[ $commands[kubectl] ]] && source <(kubectl completion zsh)
+
+# Instead of using `source` on every startup, permanently install with below commands.
+# User dir may be swapped for system dir `/usr/local/share/zsh/site-functions/` to make
+# completions available to all users.
+#
+# Add completion functions to user dir for each command.
+# This is only needed once or whenever an update is needed.
+# Do not put this in startup script.
+#
+# ```
+# [[ $commands[jj] ]] && COMPLETE=zsh jj > ~/.local/share/zsh/site-functions/_jj
+# ```
+#
+# Add user dir to `fpath` so completion functions are loaded automatically on startup.
+# Must run before `compinit`.
+
+fpath=(
+    ~/.local/share/zsh/site-functions/
+    $fpath
+)
+
 # The following lines were added by compinstall
 zstyle :compinstall filename '/home/quietengineer/.zshrc'
 
